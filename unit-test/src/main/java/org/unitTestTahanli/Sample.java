e4037ec3f8c502b1a0db25b52b0a524480fbb3c3
Alihan <92430766+kh4n-zg@users.noreply.github.com>
2023-04-02 12:58:07 +0300

    Add files via upload

---
+++ unit-test/src/main/java/org/unitTestTahanli/Sample.java
@@ -0,0 +1,20 @@
+package org.unitTestTahanli;
+
+public class Sample {
+
+    public String getMyName(){
+        return "Alihan Özgöç";
+    }
+
+    public int addTwoNumber(int a,int b){
+        return a+b;
+    }
+
+    public boolean isPositive(int number){
+        return number > 0;
+    }
+
+    public String[] getTokens(String text, String separator){
+        return text.split(separator);
+    }
+}
